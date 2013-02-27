1bc2e51119b1383312103631bca1577734a95013
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-27 12:23:31 +0100

    added rack settings

--- lib/watch.rb
+++ lib/watch.rb
@@ -1,10 +1,30 @@
 require "watch/version"
 require 'sinatra'
 require 'sinatra/base'
+require 'sinatra-websocket'
+
 
 set :root,  File.expand_path('..', __FILE__)
 set :views, Proc.new { File.join(root, "views") }
+set :server, 'thin'
+set :sockets, []
 
-get "/" do
-	erb :index
+get '/' do
+  if !request.websocket?
+    erb :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("wetbsocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
 end
