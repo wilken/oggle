08e6d2072da449495bd8e40fc1529254212f3616
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-27 14:05:57 +0100

    added rack settings

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'thor'
 require 'watch'
 
+set :run, false
+
 class CLI < Thor
   desc "start", "Start the watch server."
   def start(filename=nil)
--- lib/watch.rb
+++ lib/watch.rb
@@ -1,8 +1,8 @@
 require "watch/version"
-require 'sinatra'
 require 'sinatra/base'
 require 'sinatra-websocket'
 require 'pry'
+
 
 module Watch 
 	class App < Sinatra::Base
@@ -10,26 +10,11 @@ module Watch
 		set :views, Proc.new { File.join(root, "views") }
 		set :server, 'thin'
 		set :sockets, []
-		
+		set :run, false
+
 		get '/index.html' do
-			if !request.websocket?
-		  		p "#{$servers} goog"
-	    		#erb :index
-	  		else
-	 			request.websocket do |ws|
-	    			ws.onopen do
-	        			ws.send("Hello World!")
-	        		end
-	        		settings.sockets << ws
-	        	end
-	      	end
-	      	ws.onmessage do |msg|
-	        	EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-	      	end
-	      	ws.onclose do
-	        	warn("wetbsocket closed")
-	        	settings.sockets.delete(ws)
-	    	end
+	  		p "#{$servers} goog"
+    		erb :index
 		end
 	end
 end
