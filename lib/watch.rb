b1e1812810383bab013c70b82e257a74ec540db0
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-27 10:29:33 +0100

    added rack settings

--- lib/watch.rb
+++ lib/watch.rb
@@ -2,9 +2,10 @@ require "watch/version"
 require 'sinatra'
 require 'sinatra/base'
 
-enable :run
-set :views,  File.expand_path('../views', __FILE__)
+enable :run if __FILE__ == $0
 
+set :root,  File.expand_path('..', __FILE__)
+set :views, 'views'
 get "/" do
 	erb :index
 end
