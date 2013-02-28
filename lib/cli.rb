ccbd90ff90c18ba1bda131efedf599859cac3afb
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:20:29 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,6 +8,7 @@ class CLI < Thor
   desc "start FILE", "Start the watch server watching files in FILE."
   method_option :port,:aliases => "-p", :desc => "Webserver port"
   def start(file=nil)
+  	p options
 	Watch::App.run!(
   		servers:{
   			servers:'foo',
