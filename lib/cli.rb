bd445fce6225f8b1ac77a40917b08034d0436fe3
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:55:47 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,20 +3,18 @@ require 'thor'
 require 'watch'
 require 'yaml'
 
-class CLI < Thor
-  desc "start FILE", "Start the watch server watching files in FILE."
-  method_option :port,:aliases => "-p", :desc => "Webserver port"
-  def start(file=nil)
-	servers = YAML.load_file(file)
-	Watch::App.run!(
-  		servers:{
-  			servers['servers']
-  		},
-		port:options[:port]||4567,
-		run:false
-
-	)
-  end
+module Watch
+	class CLI < Thor
+	  desc "start FILE", "Start the watch server watching files in FILE."
+	  method_option :port,:aliases => "-p", :desc => "Webserver port"
+	  def start(file=nil)
+		#servers = YAML.load_file(file)
+		Watch::App.run!(
+	  		port: options[:port]||4567,
+			run: false
+		)
+	  end
+	end
 end
 
-CLI.start(ARGV)
+Watch::CLI.start(ARGV)
