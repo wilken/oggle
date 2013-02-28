da240a2815abfb44401874ef000ec3a3aad8a97e
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:30:39 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,7 +10,7 @@ class CLI < Thor
   method_option :port,:aliases => "-p", :desc => "Webserver port"
   def start(file=nil)
   	p options
-	Watch::App.doptions.merge!(
+	Watch::App.options.merge!(
   		:run  => false,
   		:env  => :production,
   		:port => option[:port]||8000
