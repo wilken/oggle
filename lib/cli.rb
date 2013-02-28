256f42f51217576a573920ed47990ab45f5a0a64
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:56:36 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,8 +8,9 @@ module Watch
 	  desc "start FILE", "Start the watch server watching files in FILE."
 	  method_option :port,:aliases => "-p", :desc => "Webserver port"
 	  def start(file=nil)
-		#servers = YAML.load_file(file)
+		servers = YAML.load_file(file)
 		Watch::App.run!(
+
 	  		port: options[:port]||4567,
 			run: false
 		)
