1e17698293e1c5526cfc8e623161cf52ff18832a
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:14:50 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,11 +6,13 @@ set :run, false
 
 class CLI < Thor
   desc "start", "Start the watch server."
+  option :port
   def start(filename=nil)
-
-
 	Watch::App.run!(
-  		servers:{servers:'foo'}
+  		servers:{
+  			servers:'foo',
+  			port:options[:port]||4567
+  		}
 	)
   end
 end
