3c05e3c4fa452e3c0d37b28c30a99440e88c1ce3
unknown <z8wmw@H40841.intern.kmd.dk>
2013-02-28 07:34:15 +0100

    ..

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,21 +2,18 @@ require 'sinatra'
 require 'thor'
 require 'watch'
 
-set :run, false
-set :port, 8000
-
 class CLI < Thor
   desc "start FILE", "Start the watch server watching files in FILE."
   method_option :port,:aliases => "-p", :desc => "Webserver port"
   def start(file=nil)
   	p options
-  	p Watch::App.options
-	Watch::App.options.merge!(
-  		:run  => false,
-  		:env  => :production,
-  		:port => option[:port]||8000
+	Watch::App.run!(
+  		servers:{
+  			servers:'foo',
+  			port:8000,
+  			run:false
+  		}
 	)
-	run Watch::App
   end
 end
 
