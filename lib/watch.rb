fa0001d67e57c64d1d5db2315b005acaca6eda1b
unknown <z8wmw@H40841.intern.kmd.dk>
2013-03-04 12:31:57 +0100

    ..

--- lib/watch.rb
+++ lib/watch.rb
@@ -32,7 +32,7 @@ module Watch
 			else
 			    request.websocket do |ws|
 			    	ws.onopen do
-			        	ws.send("{\"init\" : #{settings.servers.to_s}}")
+			        	ws.send("{\"init\" : #{settings.servers.to_json}}")
 			        	settings.sockets << ws
 			      	end
 			      	ws.onmessage do |msg|
