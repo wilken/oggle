58d25b744ebbbad2aa029e32dd6562482f194790
unknown <z8wmw@H40841.intern.kmd.dk>
2013-03-04 11:49:10 +0100

    ..

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ watch.factory('websocket', function($rootScope) {
         },
         send: ws.send,
         onopen:function(fn) {
+            console.log("set onopen")
             ws.onopen = function() {
                 $rootScope.$apply(function() {
                     fn();
