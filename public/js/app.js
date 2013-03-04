29972510be41ffefceb09d0fef50aa46a66572a0
unknown <z8wmw@H40841.intern.kmd.dk>
2013-03-04 11:50:22 +0100

    ..

--- public/js/app.js
+++ public/js/app.js
@@ -32,15 +32,15 @@ watch.factory('websocket', function($rootScope) {
 
 watch.controller('watchCtrl', function($scope, websocket) {
     console.log("starting controller")
-    websocket.onmessage = function(evt) {
+    websocket.onmessage(function(evt) {
         console.log(evt)
-    };
-    websocket.onclose = function() { 
+    })
+    websocket.onclose(function() { 
         console.log("socket closed"); 
-    };
-    websocket.onopen = function() { 
+    })
+    websocket.onopen(function() { 
         console.log("connected...");
         websocket.send("hello server");
-    };
+    })
 
 })
