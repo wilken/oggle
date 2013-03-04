618288cd7669cd90c7a037fe09e2bd0bfd369d98
unknown <z8wmw@H40841.intern.kmd.dk>
2013-03-04 10:49:24 +0100

    ..

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+var watch = angular.module('watch', [])
+watch.factory('websocket', function($rootScope) {
+    var ws = new WebSocket("ws://localhost");
+    ws.onmessage = function(evt) { $("#msg").append("<p>"+evt.data+"</p>"); };
+    ws.onclose = function() { debug("socket closed"); };
+    ws.onopen = function() { 
+        debug("connected...");
+        ws.send("hello server");
+    };
+    return {
+        onmessage: function(evt, fn) {
+            ws.onmessage = function(evt) {
+                $rootScope.$apply(function() {
+                    fn(evt);
+                });
+            }
+        },
+        send: ws.send,
+        onopen; ws.onopen.
+        onclose: ws.onclose
+    }
+})
+
+watch.controller('watchCtrl', function($scope, $websocket) {
+    $websocket.onmessage = function(evt) {
+        console.log(evt)
+    };
+    $websocket.onclose = function() { 
+        console.log("socket closed"); 
+    };
+    $websocket.onopen = function() { 
+        console.log("connected...");
+        $websocket.send("hello server");
+    };
+
+})
